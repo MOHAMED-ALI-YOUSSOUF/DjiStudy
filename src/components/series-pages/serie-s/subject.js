abf922a49078ab810cfe77f052b9a6f270cc8c32
MOHAMED-ALI-YOUSSOUF <wizzimed@gmail.com>
2024-05-09 23:35:11 +0300

    v1

--- src/components/series-pages/serie-s/subject.js
+++ src/components/series-pages/serie-s/subject.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const Subject = ({ pdfURL }) => {
   return (
-    <div>
-      <iframe src={pdfURL} title="PDF Viewer" width="100%" height="600px"></iframe>
-    </div>
+    <Container className="d-flex justify-content-center">
+      {" "}
+      <div className="w-50 d-flex justify-content-center">
+        <iframe
+          src={pdfURL}
+          title="PDF Viewer"
+          width="100%"
+          height="600px"
+        ></iframe>
+      </div>
+      ;
+    </Container>
   );
-}
+};
 
 export default Subject;
